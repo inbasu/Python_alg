725ef5345842a98b6a0cba80b02142d81768b0f9
--I <just.me.juncy@gmail.com>
2022-04-06 08:38:42 +0400

    Update #1.py

--- lesson_8/#1.py
+++ lesson_8/#1.py
@@ -4,13 +4,12 @@ import hashlib
 def comp(string):
     h_string = hashlib.sha1(string.encode('utf-8')).hexdigest()
     h_dif_string = []
-    for _ in range(len(string)):
-        for i in range(len(string)):
-            for j in range(len(string)-1):
-                if i+j > len(string):
-                    break
-                h_sub = hashlib.sha1(string[i:i+j].encode('utf-8')).hexdigest()
-                h_dif_string.append(h_sub)
+    for i in range(len(string)):
+        for j in range(len(string)):
+            if i+j > len(string):
+                break
+            h_sub = hashlib.sha1(string[i:i+j].encode('utf-8')).hexdigest()
+            h_dif_string.append(h_sub)
 
     # print(f'Hash  {h_string}; подстрок  {len(h_dif_string)}')
     return len(set(h_dif_string))
